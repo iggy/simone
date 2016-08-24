7e0e0dd553d8c41d4a4361ccf6c0aa2698317894
Iggy <iggy@theiggy.com>
2016-08-24 12:52:20 -0400

    setup.py enhancements

--- setup.py
+++ setup.py
@@ -1,19 +1,88 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from shutil import rmtree
+# from cartridge import __version__ as version
+version = "0.1.1"
+
+#exclude = ["cartridge/project_template/dev.db",
+#           "cartridge/project_template/project_name/local_settings.py"]
+if sys.argv == ["setup.py", "test"]:
+    exclude = []
+exclude = dict([(e, None) for e in exclude])
+for e in exclude:
+    if e.endswith(".py"):
+        try:
+            os.remove("%sc" % e)
+        except:
+            pass
+    try:
+        with open(e, "r") as f:
+            exclude[e] = (f.read(), os.stat(e))
+        os.remove(e)
+    except:
+        pass
+
+if sys.argv[:2] == ["setup.py", "bdist_wheel"]:
+    # Remove previous build dir when creating a wheel build,
+    # since if files have been removed from the project,
+    # they'll still be cached in the build dir and end up
+    # as part of the build, which is really neat!
+    try:
+        rmtree("build")
+    except:
+        pass
+
+
 try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+    setup(
 
-config = {
-    'description': 'Simone - Django Webmail App',
-    'author': 'Iggy',
-    'url': 'https://github.com/iggy/simone',
-    'download_url': 'https://github.com/iggy/simone',
-    'author_email': 'iggy@theiggy.com',
-    'version': '0.1.0',
-    'install_requires': [],
-    'packages': ['simone'],
-    'scripts': [],
-    'name': 'simone'
-}
+        name="Simone",
+        version=version,
+        author="Iggy",
+        author_email="iggy@theiggy.com",
+        description="A Django webmail application.",
+        long_description=open("README.rst", 'rb').read().decode('utf-8'),
+        license="BSD",
+        url="http://github.com/iggy/simone/",
+        zip_safe=False,
+        include_package_data=True,
+        packages=find_packages(),
+        test_suite="runtests.main",
+        tests_require=["pyflakes>=0.6.1", "pep8>=1.4.1"],
 
-setup(**config)
+        install_requires=[
+            "django>=1.9",
+        ],
+
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "Environment :: Web Environment",
+            "Framework :: Django",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Communications :: Email :: Email Clients (MUA)",
+            "Topic :: Software Development :: Libraries :: "
+                                                "Application Frameworks",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ])
+
+finally:
+    for e in exclude:
+        if exclude[e] is not None:
+            data, stat = exclude[e]
+            try:
+                with open(e, "w") as f:
+                    f.write(data)
+                os.chown(e, stat.st_uid, stat.st_gid)
+                os.chmod(e, stat.st_mode)
+            except:
+                pass
