e2077238e2e31d7091d98f9eeb7f225f21f4aea7
iggy <iggy@theiggy.com>
2016-11-12 09:12:16 -0800

    switch back to dialog for now

--- simone/static/js/common.js
+++ simone/static/js/common.js
@@ -301,10 +301,27 @@ $(document).ready(function() {
                         height: dlgh
                     };
 
-                    modalHTML = ''
+                    modalHTML = '<div class="modal fade" tabindex="-1" role="dialog"> \
+  <div class="modal-dialog" role="document"> \
+    <div class="modal-content"> \
+      <div class="modal-header"> \
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button> \
+        <h4 class="modal-title">Modal title</h4> \
+      </div> \
+      <div class="modal-body"> \
+        <p>One fine body&hellip;</p> \
+      </div> \
+      <div class="modal-footer"> \
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> \
+        <button type="button" class="btn btn-primary">Save changes</button> \
+      </div> \
+    </div><!-- /.modal-content --> \
+  </div><!-- /.modal-dialog --> \
+</div><!-- /.modal -->'
 
                     dw.dialog.viewmsg = $('<div class=""></div>').append('body');
-                    $(dw.dialog.viewmsg).load('viewmsg/' + server + '/' + folder + '/' + uid + '/').modal(options);
+                    // TODO s/dialog/modal/ to bootstrapify this
+                    $(dw.dialog.viewmsg).load('viewmsg/' + server + '/' + folder + '/' + uid + '/').dialog(options);
 
                     $(this).parent().removeClass('msgunseen');
                     $(this).parent().addClass('msgseen');
